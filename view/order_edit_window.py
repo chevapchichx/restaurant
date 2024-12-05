8730d8f8c5d28282238814244e037661e0204014
chevapchichx <tugaisveta@gmail.com>
2024-12-06 00:17:25 +0300

    no god in this room

--- view/order_edit_window.py
+++ view/order_edit_window.py
@@ -26,10 +26,12 @@ class Order_Edit_Window(QDialog):
         main_layout.setContentsMargins(10, 20, 10, 10)
 
         top_layout = QVBoxLayout()
-        self.table_label = QLabel(f"Стол: {self.order.table.id_table}, Количество гостей: {self.order.guests}\n\nБлюда:")
+        self.table_label = QLabel(f"Стол: {self.order.table.id_table}, Количество гостей: {self.order.guests}")
+        self.dish_label = QLabel("Блюда:")
         self.table_label.setAlignment(Qt.AlignmentFlag.AlignCenter | Qt.AlignmentFlag.AlignVCenter)
         top_layout.addWidget(self.table_label)
-        top_layout.addStretch()
+        top_layout.addWidget(self.dish_label)
+        # top_layout.addStretch()
         main_layout.addLayout(top_layout)
 
         self.dish_info_layout = QGridLayout()
@@ -38,7 +40,7 @@ class Order_Edit_Window(QDialog):
         
         bottom_layout = QHBoxLayout()
         self.add_dish_label = QLabel("Добавить блюдо:")
-        self.add_dish_label.setAlignment(Qt.AlignmentFlag.AlignCenter | Qt.AlignmentFlag.AlignVCenter)
+        # self.add_dish_label.setAlignment(Qt.AlignmentFlag.AlignCenter | Qt.AlignmentFlag.AlignVCenter)
         bottom_layout.addWidget(self.add_dish_label)
 
         self.add_dish_layout = QGridLayout()
@@ -68,12 +70,12 @@ class Order_Edit_Window(QDialog):
         self.back_button.clicked.connect(lambda: open_order_list_window(self))
         button_layout.addWidget(self.back_button)
 
-        main_layout.addStretch(0) 
+        # main_layout.addStretch(0) 
         main_layout.addLayout(self.dish_info_layout)
-        main_layout.addStretch(1)  
+        # main_layout.addStretch(0)  
 
         main_layout.addLayout(bottom_layout)
-        main_layout.addStretch(2)
+        # main_layout.addStretch(2)
         main_layout.addLayout(self.add_dish_layout)
 
         main_layout.addLayout(button_layout)
@@ -102,7 +104,6 @@ class Order_Edit_Window(QDialog):
 
             # if self.user.role == User_Role.ADMIN:
             #     self.dish_info_layout.addWidget(delete_button, i, 3)
-            
 
             if dish.status == Dish_Status.CREATED:
                 spinbox = QSpinBox()
@@ -121,6 +122,6 @@ class Order_Edit_Window(QDialog):
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    window = Order_Edit_Window(2)  
+    window = Order_Edit_Window(1)  
     window.show()
     sys.exit(app.exec())
